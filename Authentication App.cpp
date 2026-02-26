5e93cda45e4f7c8e4e0d89a7c978240b644653a2
Bladev7 <aimanahmed808003@gmail.com>
2026-02-26 21:49:08 +0300

    update : added windows API call to auto open the picture for the created qr code

--- Authentication App.cpp
+++ Authentication App.cpp
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <random>
 #include <vector>
+
 #include <ZXing/BitMatrix.h>
 #include <ZXing/BitMatrixIO.h>
 #include <ZXing/MultiFormatWriter.h>
@@ -11,7 +12,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image_write.h>
 #include <stb_image.h>
-
+#include <windows.h>
 
 
 using namespace std;
@@ -51,6 +52,13 @@ string conv_toBase32(vector<unsigned char> key ) //Base32 Encoder
 	return result;
 }
 
+string decode_Base32() //Base32 Decoder
+{
+
+
+	return "0";
+}
+
 vector <unsigned char> GenerateRandom()
 {
 	
@@ -63,9 +71,7 @@ vector <unsigned char> GenerateRandom()
 	for (int i = 0; i < 16; i++)
 	{
 		secret.push_back(static_cast<unsigned char>(randombyte(gen) ) ); // converting integers we get from the generator of random numbers into unsigned char 
-
 	}
-	
 	return secret;
 }
 
@@ -77,7 +83,11 @@ string TOTP_URI(const string Account ,const string Key ,const string AppName)
 
 bool GenerateQRCode(const string & URI, const string & fileName, const int size )
 {
+
 	vector<unsigned char> pixels(size * size); //(height * width)
+	string pngName;
+	string pngPath;
+
 
 	MultiFormatWriter writer(BarcodeFormat::QRCode); //creating a class Member that generate QRCode
 	BitMatrix QRMatrix = writer.encode(URI,size,size); //encode the data into QR Code Matrix
@@ -92,7 +102,7 @@ bool GenerateQRCode(const string & URI, const string & fileName, const int size 
 	
 	if (img_check) 
 	{
-		string pngName = fileName;
+		pngName = fileName;
 		pngName.erase(pngName.size() - 4); //deleting the .pbm in the filename when converting it to png
 
 		// save as PNG
@@ -112,20 +122,86 @@ bool GenerateQRCode(const string & URI, const string & fileName, const int size 
 		cerr << "Failed to load pbm file " << endl; //using cerr to detect errors
 		return false;
 	}
+	
+	//user choice
+	int user_choice;
+
+	
+
+	pngPath = pngName + ".png";
+
+
+	cout << "QR Code :" << endl;
+	cout << "1-open png file \n" <<"2-print in cmd"<<endl;
+	cin >> user_choice;
+	
+	if (user_choice == 1)
+	{
+		if (GetFileAttributesA(pngPath.c_str()) == INVALID_FILE_ATTRIBUTES)
+		{
+			cerr << "PNG file not found" << endl;
+			return false;
+		}
+
+		HINSTANCE result = ShellExecuteA(
+			NULL,            // no parent window
+			"open",          // operation
+			pngPath.c_str(), // file to open and c_str() convert string to const char for Windows API to handle
+			NULL,            // parameters
+			NULL,            // default directory
+			SW_SHOWNORMAL    // show normally
+		);
+
+		if ((int)result <= 32) // Microsoft error codes int>32 if int>32 success
+		{
+			cerr << "Failed to open PNG file" << endl;
+			return false;
+		}
+
+	}
+
+	else if (user_choice == 2)
+	{
+		int modules = QRMatrix.width();   // e.g., 33 or 41
+		int targetWidth = 40;             // try between 25–40
+		int scale = ceil(modules / (double)targetWidth);
+
+		for (int y = 0; y < modules; y += scale)
+		{
+			for (int x = 0; x < modules; x += scale)
+			{
+				if (QRMatrix.get(x, y))
+				{
+					cout << u8"\u2588\u2588";  // black module
+				}
+				else
+				{
+					cout << "  ";   // white module
+				}
+			}
+			cout << endl; // new line after each row
+		}
+
+	}
+
+	else
+	cout << "invalid input";
+
 	return true;
 }
 
 
 int main()
 {
+	SetConsoleOutputCP(CP_UTF8);
+	
 	vector<unsigned char> secretKey = GenerateRandom();
 	string Encoded_secret = conv_toBase32(secretKey);
 	string URI = TOTP_URI("user@example.com" , "My2faApp", Encoded_secret);
-	GenerateQRCode(URI ,"C:\\Users\\user\\Documents\\QRcode.pbm",200);
-	
 	cout << "2-Authentication App" << endl;
-	
-	
-	system("pause");  // waits for the user to press any key
+
+	GenerateQRCode(URI ,"C:\\Users\\user\\Documents\\QRcode.pbm",200);
+
+	system("pause");  
 	return 0;
 }
